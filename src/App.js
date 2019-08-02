93ea823adc0c1c9e337751db559fbd32dd98908f
youssef samih <youssef.samih97@gmail.com>
2019-08-03 00:50:33 +0100

    style card and card list

--- src/App.js
+++ src/App.js
@@ -19,13 +19,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <CardList name='you'>
-        {
-          this.state.monsters.map(monster => (
-            <h1 key={ monster.id }> { monster.name } </h1>)
-          )
-        }
-        </CardList>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Card } from '../card/card.component'
 import './card-list.styles.css'
 
 export const CardList = props => {
     return (
         <div className="card-list">
-            { props.children }
+            {
+                props.monsters.map(monster => (
+                        <Card 
+                            key={ monster.id } 
+                            monster={monster}
+                        />
+                    )
+                )
+            }
         </div>
     )
 }
