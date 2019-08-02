fbf777348c433ca6c58b2439d3a50cc6e1b96266
youssef samih <youssef.samih97@gmail.com>
2019-08-02 23:58:40 +0100

    card list component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component'
 
 class App extends Component {
   constructor(){
@@ -19,11 +19,13 @@ class App extends Component {
   render(){
     return (
       <div className="App">
+        <CardList name='you'>
         {
           this.state.monsters.map(monster => (
             <h1 key={ monster.id }> { monster.name } </h1>)
           )
         }
+        </CardList>
       </div>
     );
   }
