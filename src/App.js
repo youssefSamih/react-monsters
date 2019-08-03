2cae464473dbc1075fae458b51348d8340afaef6
youssef samih <youssef.samih97@gmail.com>
2019-08-03 03:11:03 +0100

    setup search boc component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -17,9 +19,17 @@ class App extends Component {
   }
   
   render(){
+    const { monsters, searchField } = this.state
+    const filtetredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())  
+    )
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox 
+          placeholder="search monsters"
+          handleChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filtetredMonsters} />
       </div>
     );
   }
