6fe50efb5c222c71898b18ce5375eef7bd7505b1
youssef samih <youssef.samih97@gmail.com>
2019-08-05 00:12:41 +0100

    adding files

--- src/App.js
+++ src/App.js
@@ -10,12 +10,17 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+    // this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
   }
   
   render(){
@@ -25,9 +30,10 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filtetredMonsters} />
       </div>
