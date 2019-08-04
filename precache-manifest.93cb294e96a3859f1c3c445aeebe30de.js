a707f6ab0c16941dace741f24e7fcbfe3156eec0
youssef samih <youssef.samih97@gmail.com>
2019-08-05 00:06:27 +0100

    Updates

---
+++ precache-manifest.93cb294e96a3859f1c3c445aeebe30de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586454e3a03cb33e3ed125a955bde101",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "3b5f6a4637b272119537",
+    "url": "/react-monsters/static/css/main.f24c1f6a.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/react-monsters/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "3b5f6a4637b272119537",
+    "url": "/react-monsters/static/js/main.9f85d191.chunk.js"
+  },
+  {
+    "revision": "1f3a671e84167858cec3",
+    "url": "/react-monsters/static/js/runtime~main.3e155ff7.js"
+  }
+]);
